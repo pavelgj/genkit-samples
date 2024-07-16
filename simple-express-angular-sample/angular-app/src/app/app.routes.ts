4536fd9b901ba69facf9f87875a5477b72b2e50d
pavelj <pavelj@google.com>
2024-07-16 15:18:18 +0000

    break up the app into routes

--- simple-express-angular-sample/angular-app/src/app/app.routes.ts
+++ simple-express-angular-sample/angular-app/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MenuComponent } from './menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'menu',
+        component: MenuComponent,
+    },
+    {
+        path: 'contact',
+        component: ContactUsComponent,
+    },
+    { path: '**', redirectTo: '/home' },
+
+];
